f4dfe9da0c6f40f0ebbc69e57d830fbce5dd0bda
Jmo5896 <jmo5896@moore-clan.net>
2018-06-27 20:07:37 -0600

    used flexbox to get the images to line up in rows, working on the click function

--- clicky-react-game/src/App.js
+++ clicky-react-game/src/App.js
@@ -1,29 +1,56 @@
 import React, { Component } from 'react';
-import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Picture from './components/Picture';
 import images from './components/images.json';
 
 class App extends Component {
   state = {
-    images
+    images: images,
+    score: 0,
+    top_score: 0
   }
+
+  
+  switchClick = () => {
+    
+      this.setState({ score: this.state.score + 1}) ;
+      // this.setState({ score: newScore });
+      // let changeClick = this.state.clicked;
+      // this.setState(changeClick => {
+      //   images[0].clicked = true;
+      // });
+    
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+            <div className = 'col-sm-4'>
+              <a className="navbar-brand" href="/">
+                <h3>Clicky Game!</h3>            
+              </a>
+            </div>
+            <div className = 'col-sm-4'>
+              <h3>Click an image to begin</h3>
+            </div>
+            <div className = 'col-sm-4'>
+              <h3>Score: {this.state.score} | Top Score: {this.state.top_score}</h3>
+            </div>
+          </nav>
         <div className = 'container'>
-          <div clasName = 'row'>
+          <div className = 'flex-container'>
               {this.state.images.map(pic => (
                 <Picture 
                   id = {pic.id}
                   key = {pic.id}
                   image = {pic.image}
+                  onClick = {this.switchClick()}
                 />
               ))}
           </div>
-        </div>
-          
+        </div> 
         <Footer />
       </div>
     );
--- clicky-react-game/src/components/Picture/Picture.js
+++ clicky-react-game/src/components/Picture/Picture.js
@@ -2,12 +2,11 @@ import React from 'react';
 import './Picture.css';
 
 
+
 const Picture = props => (
-    <div className = 'col-sm-1'>
-        <div className = 'card'>
+        <div className = 'card' value = {props.clicked} >
             <img alt = {props.name} src = {props.image} />
-        </div>
-    </div>    
+        </div>  
         
     
 )
